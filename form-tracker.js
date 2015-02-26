06115115096d29455428cd5f74d7686f5270955a
Wolf <wolf.n@omcg.de>
2015-02-26 14:58:31 +0100

    Update for Universal Analytics

--- form-tracker.js
+++ form-tracker.js
@@ -2,10 +2,12 @@
 $(document).ready(function() { 
 	$(':input').blur(function () {
 		if($(this).val().length > 0) {
-			_gaq.push(['_trackEvent', 'INSERT FORM NAME HERE', 'completed', $(this).attr('name')]);
+			// Old Analytics  _gaq.push(['_trackEvent', 'INSERT FORM NAME HERE', 'completed', $(this).attr('name')]);
+			ga(‘send’, ‘event’, ‘INSERT FORM NAME HERE’, ‘completed’, $(this).attr(‘name’));
 		} 
 		else {
-			_gaq.push(['_trackEvent', 'INSERT FORM NAME HERE', 'skipped', $(this).attr('name')]);
+			// Old Analytics  _gaq.push(['_trackEvent', 'INSERT FORM NAME HERE', 'skipped', $(this).attr('name')]);
+			ga(‘send’, ‘event’, ‘INSERT FORM NAME HERE’, ‘sipped’, $(this).attr(‘name’));
 		}
 	});
 });
